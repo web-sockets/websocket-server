4513b18956f9ead1eecbd75e54535bcb8e0685c4
Pratap A K <pratap.ak@ge.com>
2019-08-22 08:37:11 +0530

    getting users for subscriptions

--- src/main/java/com/websocketserver/controller/UserController.java
+++ src/main/java/com/websocketserver/controller/UserController.java
@@ -2,30 +2,50 @@ package com.websocketserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.user.SimpSubscription;
+import org.springframework.messaging.simp.user.SimpSubscriptionMatcher;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @RestController
 public class UserController {
 
+    private final SimpUserRegistry simpUserRegistry;
+
+    private final SimpMessagingTemplate template;
+
     @Autowired
-    private SimpMessagingTemplate template;
+    public UserController(SimpUserRegistry simpUserRegistry, SimpMessagingTemplate template) {
+        this.simpUserRegistry = simpUserRegistry;
+        this.template = template;
+    }
 
     @RequestMapping(path = "/user", method = RequestMethod.GET)
     public String get() {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        /*Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username;
         if (principal instanceof UserDetails) {
             username = ((UserDetails) principal).getUsername();
         } else {
             username = principal.toString();
-        }
-        System.out.println(">>>>>>>>>>>>");
-        System.out.println("username= " + username);
-        template.convertAndSendToUser(username, "/queue/1", "hello " + username);
+        }*/
+
+        Set<SimpSubscription> sub = simpUserRegistry
+                .findSubscriptions(s -> s.getDestination().equals("/user/queue/1"));
+        sub.forEach(s -> {
+            System.out.println(s.getDestination());
+            System.out.println(s.getId());
+            System.out.println(s.getSession());
+            String username = s.getSession().getUser().getName();
+            template.convertAndSendToUser(username, "/queue/1", "hello " + username);
+        });
         return "hello user";
     }
 }
