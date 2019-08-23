7b91cf7484d4faa96d49ca32af1534184cc2394f
Pratap A K <pratap.ak@ge.com>
2019-08-23 09:16:03 +0530

    code to use full featured broker like rabbitmq

--- src/main/java/com/websocketserver/config/WebSocketConfig.java
+++ src/main/java/com/websocketserver/config/WebSocketConfig.java
@@ -12,8 +12,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-         // Note: Using queue, not topic, Spring always using queue with sendToUser
+        // Note: Using queue, not topic, Spring always using queue with sendToUser
         registry.enableSimpleBroker("/queue");
+        /*
+          Check the STOMP documentation for your message broker of choice (e.g. RabbitMQ, ActiveMQ, etc.),
+          install the broker, and run it with STOMP support enabled.
+          Then enable the STOMP broker relay in the Spring configuration instead of the
+          simple broker.
+         */
+        /*registry.enableStompBrokerRelay("/topic", "/queue")
+        .setRelayHost("localhost")
+        .setRelayPort(61613)
+        .setClientLogin("guest")
+        .setClientPasscode("guest");*/
         registry.setApplicationDestinationPrefixes("/app");
     }
 
